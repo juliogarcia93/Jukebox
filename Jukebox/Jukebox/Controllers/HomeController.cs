d7850b052766030ff0bb7f05e8538a454ffe0d45
Daryl <ZOMGitsDaryl@gmail.com>
2013-10-10 12:19:35 -0700

    Edited the homepage, contacts, and about page.

--- Jukebox/Jukebox/Controllers/HomeController.cs
+++ Jukebox/Jukebox/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Jukebox.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "A little about Jukebox";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contacts";
 
             return View();
         }
