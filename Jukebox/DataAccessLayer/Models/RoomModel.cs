115939f8cd6459e046fe6aea8015be06dd4b9b45
Christina Morris <cmmorris@umail.ucsb.edu>
2013-10-23 23:39:18 -0700

    Added RoomModel.cs

---
+++ Jukebox/DataAccessLayer/Models/RoomModel.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DataAccessLayer.Models
+{
+    class RoomModel
+    {
+        public RoomModel ()
+        {
+            RoomName = null;
+        }
+        public RoomModel(string roomname)
+        {
+            RoomName = roomname;
+        }
+        public string RoomName { get; set; }
+    }
+}
